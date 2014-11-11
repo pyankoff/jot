ef4b9aae9a1a9092a15bca45ec6e7b31a1cc927a
pyankoff <pyankoff@gmail.com>
2014-11-11 18:56:25 +0300

    Initial Commit

---
+++ jot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jot
+//
+//  Created by Andrey Pyankov on 11/11/14.
+//  Copyright (c) 2014 AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jot
+//
+//  Created by Andrey Pyankov on 11/11/14.
+//  Copyright (c) 2014 AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
